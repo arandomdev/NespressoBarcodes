bcda115275ea43fa789646d01648e19798935258
Hao Wang <haow6449@gmail.com>
2021-07-22 16:09:52 -0400

    update

--- generator.py
+++ generator.py
@@ -1,60 +1,48 @@
+from functools import partial
 
-
-SEG1 = "1100"
-SEG2 = "1101"
-SEG3 = "1011"
-SEG4 = "010110"
+# 4 segments in order, seperated by a space
+SEGMENTS = "1100 1101 0100 001101"
 
 
 def main():
 	code = ""
 
+	seg1, seg2, seg3, seg4 = SEGMENTS.split(" ")
+
 	formatter = "01{seg1}{sep1}{seg2}{sep2}{seg3}{sep3}{seg4}{sep4}"
-	code += formatter.format(
-		seg1=SEG1,
-		seg2=SEG2,
-		seg3=SEG3,
-		seg4=SEG4,
+	formatter = partial(
+		formatter.format,
+		seg1=seg1,
+		seg2=seg2,
+		seg3=seg3,
+		seg4=seg4
+	)
+
+	code += formatter(
 		sep1="10",
 		sep2="10",
 		sep3="10",
 		sep4="01",
 	)
-	code += formatter.format(
-		seg1=SEG1,
-		seg2=SEG2,
-		seg3=SEG3,
-		seg4=SEG4,
+	code += formatter(
 		sep1="10",
 		sep2="01",
 		sep3="01",
 		sep4="10",
 	)
-	code += formatter.format(
-		seg1=SEG1,
-		seg2=SEG2,
-		seg3=SEG3,
-		seg4=SEG4,
+	code += formatter(
 		sep1="01",
 		sep2="10",
 		sep3="01",
 		sep4="01",
 	)
-	code += formatter.format(
-		seg1=SEG1,
-		seg2=SEG2,
-		seg3=SEG3,
-		seg4=SEG4,
+	code += formatter(
 		sep1="01",
 		sep2="01",
 		sep3="01",
 		sep4="01",
 	)
-	code += formatter.format(
-		seg1=SEG1,
-		seg2=SEG2,
-		seg3=SEG3,
-		seg4=SEG4,
+	code += formatter(
 		sep1="01",
 		sep2="01",
 		sep3="10",
